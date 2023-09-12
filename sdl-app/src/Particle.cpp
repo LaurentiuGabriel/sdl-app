996edb848d406151e51e46449806d19696fd8848
Laurentiu Raducu <laurentiu.raducu@gmail.com>
2023-09-12 22:03:33 +0200

    Remembering stuff

--- sdl-app/src/Particle.cpp
+++ sdl-app/src/Particle.cpp
@@ -1,50 +1,45 @@
 /*
  * Particle.cpp
  *
- *  Created on: Aug 9, 2018
+ *  Created on: Aug 9, 2018 - long time ago
  *      Author: Laurentiu
  */
 
 #include "Particle.h"
-#include <math.h>
-#include <stdlib.h>
+#include <cstdlib> // Include for rand()
+#include <cmath>   // Include for M_PI
 
+class Particle {
+private:
+    double m_x;
+    double m_y;
+    double m_direction;
+    double m_speed;
 
-Particle::Particle() :
-		m_x(0), m_y(0) {
+public:
+    Particle() : m_x(0), m_y(0) {
+        init();
+    }
 
-	init();
-}
+    ~Particle() = default;
 
-void Particle::init() {
-	m_x = 0;
-	m_y = 0;
-	m_direction = (2 * M_PI * rand()) / RAND_MAX;
-	m_speed = (0.04 * rand()) / RAND_MAX;
+    void init() {
+        m_x = 0;
+        m_y = 0;
+        m_direction = (2.0 * M_PI * std::rand()) / RAND_MAX;
+        m_speed = (0.04 * std::rand()) / RAND_MAX;
+        m_speed *= m_speed;
+    }
 
-	m_speed *= m_speed;
-}
+    void update(int interval) {
+        m_direction += interval * 0.0003;
+        double xspeed = m_speed * std::cos(m_direction);
+        double yspeed = m_speed * std::sin(m_direction);
+        m_x += xspeed * interval;
+        m_y += yspeed * interval;
 
-Particle::~Particle() {
-	// TODO Auto-generated destructor stub
-}
-
-void Particle::update(int interval) {
-
-	m_direction += interval * 0.0003;
-
-	double xspeed = m_speed * cos(m_direction);
-	double yspeed = m_speed * sin(m_direction);
-
-	m_x += xspeed * interval;
-	m_y += yspeed * interval;
-
-	if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1) {
-		init();
-	}
-
-	if(rand() < RAND_MAX/100) {
-		init();
-	}
-}
-
+        if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1 || std::rand() < RAND_MAX / 100) {
+            init();
+        }
+    }
+};
