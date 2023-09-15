22b8cb43dc49769ae838cab0287f64db309387f0
Laurentiu Raducu <laurentiu.raducu@gmail.com>
2023-09-15 11:09:03 +0200

    Refactoring Swarm.cpp using vector

--- sdl-app/src/Swarm.cpp
+++ sdl-app/src/Swarm.cpp
@@ -5,27 +5,17 @@
  *      Author: Laurentiu
  */
 
+#include <vector>
 #include "Swarm.h"
 
-
-Swarm::Swarm(): lastTime(0) {
-	m_pParticles = new Particle[NPARTICLES];
-
-}
-
-Swarm::~Swarm() {
-	delete [] m_pParticles;
-}
+Swarm::Swarm() : lastTime(0), m_pParticles(std::vector<Particle>(NPARTICLES)) {}
 
 void Swarm::update(int elapsed) {
+    int interval = elapsed - lastTime;
 
-	int interval = elapsed - lastTime;
+    for (auto& particle : m_pParticles) {
+        particle.update(interval);
+    }
 
-	for (int i = 0; i < Swarm::NPARTICLES; i++) {
-		m_pParticles[i].update(interval);
-	}
-
-	lastTime = elapsed;
+    lastTime = elapsed;
 }
-
-
