b7d5f1501c8096714ebe8736d2f4568c598397c1
Laurentiu Raducu <laurentiu.raducu@gmail.com>
2023-09-17 20:46:19 +0200

    Update main.cpp

--- sdl-app/src/main.cpp
+++ sdl-app/src/main.cpp
@@ -7,69 +7,64 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
-#include <math.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include "Screen.h"
-#include <stdlib.h>
-#include <time.h>
 #include "Swarm.h"
-using namespace std;
 
+const double FREQUENCY_FACTOR_GREEN = 0.0001;
+const double FREQUENCY_FACTOR_RED = 0.0002;
+const double FREQUENCY_FACTOR_BLUE = 0.0003;
+const double SIN_OFFSET = 1.0;
+const double COLOR_SCALING = 128;
 
-int main(){
-	cout<<"Here";
-
-	srand(time(NULL));
-
-	Screen screen;
-
-
-
-	if (screen.init() == false) {
-		cout << "Error initialising SDL." << endl;
-	}
-
-
-	Swarm swarm;
-
-
-
-	while (true) {
-		// Update particles
-		// Draw particles
-
-		int elapsed = SDL_GetTicks();
-
-		swarm.update(elapsed);
-
-		unsigned char green =
-				(unsigned char) ((1 + sin(elapsed * 0.0001)) * 128);
-		unsigned char red = (unsigned char) ((1 + sin(elapsed * 0.0002)) * 128);
-		unsigned char blue = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
-
-		const Particle * const pParticles = swarm.getParticles();
-
-		for (int i = 0; i < Swarm::NPARTICLES; i++) {
-			Particle particle = pParticles[i];
-
-			int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
-			int y = particle.m_y * Screen::SCREEN_WIDTH / 2 + Screen::SCREEN_HEIGHT/2;
-
-			screen.setPixel(x, y, red, green, blue);
-		}
-
-		screen.boxBlur();
-
-		// Draw the screen
-		screen.update();
-
-		// Check for messages/events
-		if (screen.processEvents() == false) {
-			break;
-		}
-	}
-
-	screen.close();
-
-	return 0;
+unsigned char calculateColor(int elapsed, double frequencyFactor) {
+    return static_cast<unsigned char>((SIN_OFFSET + sin(elapsed * frequencyFactor)) * COLOR_SCALING);
 }
 
+void updateScreenColors(Screen &screen, Swarm &swarm, int elapsed) {
+    unsigned char green = calculateColor(elapsed, FREQUENCY_FACTOR_GREEN);
+    unsigned char red = calculateColor(elapsed, FREQUENCY_FACTOR_RED);
+    unsigned char blue = calculateColor(elapsed, FREQUENCY_FACTOR_BLUE);
+
+    const Particle * const pParticles = swarm.getParticles();
+
+    for (int i = 0; i < Swarm::NPARTICLES; i++) {
+        Particle particle = pParticles[i];
+
+        int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
+        int y = particle.m_y * Screen::SCREEN_WIDTH / 2 + Screen::SCREEN_HEIGHT / 2;
+
+        screen.setPixel(x, y, red, green, blue);
+    }
+}
+
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(NULL)));
+    Screen screen;
+
+    if (!screen.init()) {
+        std::cerr << "Error initialising SDL." << std::endl;
+        return 1; // Exit with an error code
+    }
+
+    Swarm swarm;
+
+    while (true) {
+        int elapsed = SDL_GetTicks();
+
+        swarm.update(elapsed);
+        updateScreenColors(screen, swarm, elapsed);
+
+        screen.boxBlur();
+        screen.update();
+
+        if (!screen.processEvents()) {
+            break;
+        }
+    }
+
+    screen.close();
+    return 0;
+}
